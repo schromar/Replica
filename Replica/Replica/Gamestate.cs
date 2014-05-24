ec1aa2fdd604993c11e16bf9345a404cf087954b
huebihuebner <huebner.krissi@gmail.com>
2014-05-24 18:03:41 +0200

    ?

---
+++ Replica/Replica/Gamestate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Replica
+{
+    class Gamestate
+    {
+
+        public  virtual void Update()
+        {
+        }
+        public virtual void Draw()
+        {
+        }
+
+    }
+}
