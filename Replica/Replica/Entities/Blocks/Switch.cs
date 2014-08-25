048426777b836d8dc67370d853dd85737c462d5f
fuerchter <fuerchter@gmail.com>
2014-08-25 17:16:36 +0200

    Fixed performance issues with 3D Text

--- Replica/Replica/Entities/Blocks/Switch.cs
+++ Replica/Replica/Entities/Blocks/Switch.cs
@@ -61,30 +61,29 @@ namespace Replica.Entities.Blocks
             string text = numActivated + "/" + switches.Count;
 
             //Drawing 3D Text
-            /*Matrix rotation = Matrix.Identity;
+            Matrix rotation = Matrix.Identity;
             rotation.Right = -lvl.P.T.Right;
             rotation.Up = lvl.P.T.Up;
 
-            BasicEffect textEffect = new BasicEffect(Game1.graphics.GraphicsDevice);
-            textEffect.World = rotation * Matrix.CreateScale(new Vector3(-0.075f)) * Matrix.CreateTranslation(t.position);
-            textEffect.View = effect.View;
-            textEffect.Projection = effect.Projection;
-            textEffect.TextureEnabled = true;
-            textEffect.VertexColorEnabled = true;
+            Matrix worldBackup = effect.World;
+            effect.World = rotation * Matrix.CreateScale(new Vector3(-0.075f)) * Matrix.CreateTranslation(t.position);
+            effect.TextureEnabled = true;
 
             //Having to end already open spriteBatch
             Game1.spriteBatch.End();
             Game1.graphics.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
 
-            Game1.spriteBatch.Begin(0, null, SamplerState.PointWrap, DepthStencilState.DepthRead, null, textEffect);
-
+            Game1.spriteBatch.Begin(0, null, SamplerState.PointWrap, DepthStencilState.DepthRead, null, effect);
             Vector2 textSize = Assets.font1.MeasureString(text);
             Game1.spriteBatch.DrawString(Assets.font1, text, -textSize / 2.0f, Color.White);
 
             Game1.spriteBatch.End();
             Game1.graphics.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
 
-            Game1.spriteBatch.Begin();*/
+            Game1.spriteBatch.Begin();
+
+            effect.World = worldBackup;
+            effect.TextureEnabled = false;
 
             base.Draw(graphics, gameTime, effect, camera);
         }
